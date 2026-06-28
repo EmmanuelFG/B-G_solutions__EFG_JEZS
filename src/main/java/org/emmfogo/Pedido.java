c19d3e0256a3e43370f4f6640c66f845829de9d6
EmmanuelFG <forerogomezemmanuel@gmail.com>
2026-06-28 15:44:26 -0500

    Primera interación de las clases Producto y Pedido

--- src/main/java/org/emmfogo/Pedido.java
+++ src/main/java/org/emmfogo/Pedido.java
@@ -1,4 +1,38 @@
 package org.emmfogo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+    private final List<Producto> productos;
+    private final String descuento;
+    private String id;
+    private String medioPago;
+    private int precioAcumulado;
+
+    public Pedido() {
+        this.productos = new ArrayList<>();
+        descuento = null;
+
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+        precioAcumulado += producto.getPrecio();
+
+    }
+
+
+    public void mostrarPedido() {
+        System.out.println(this.id);
+        if (productos.isEmpty()) {
+            System.out.println("El pedido se encuentra vacío");
+        } else {
+            productos.stream().forEach(Producto::mostrarProducto);
+            System.out.println("Precio Acumulado: " + precioAcumulado);
+            System.out.println("Medio de pago: ");
+            System.out.println("Descuento: ");
+        }
+
+    }
 }
--- src/main/java/org/emmfogo/Producto.java
+++ src/main/java/org/emmfogo/Producto.java
@@ -1,4 +1,18 @@
 package org.emmfogo;
 
 public class Producto {
+    private String nombre;
+    private int precio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void mostrarProducto() {
+        System.out.printf("Producto %s, precio %d%n", this.nombre, this.precio);
+    }
 }
