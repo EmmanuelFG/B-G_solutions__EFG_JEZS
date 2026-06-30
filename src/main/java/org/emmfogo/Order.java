db3895acd42933ddc0390cf7abe34ff62994a96a
juaneszuluaga857 <juaneszuluaga857@gmail.com>
2026-06-29 20:31:39 -0500

    resolviendo problemas de versiones con merge

--- src/main/java/org/emmfogo/Order.java
+++ src/main/java/org/emmfogo/Order.java
@@ -1,4 +1,41 @@
 package org.emmfogo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Order {
+    private final List<Product> products;
+    private final String discount;
+    private final int id;
+    private String paymentMethod;
+    private int totalPrice;
+
+    public Order(String paymentMethod) {
+        Random r = new Random();
+        id = r.nextInt(999);
+        this.products = new ArrayList<>();
+        discount = null;
+
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalPrice += product.getPrice();
+
+    }
+
+
+    public void showOrder() {
+        System.out.println(this.id);
+        if (products.isEmpty()) {
+            System.out.println("The order is empty");
+        } else {
+            products.stream().forEach(Product::showProduct);
+            System.out.println("Price: " + totalPrice);
+            System.out.println("Payment Method: ");
+            System.out.println("Discount: ");
+        }
+
+    }
 }
--- src/main/java/org/emmfogo/Product.java
+++ src/main/java/org/emmfogo/Product.java
@@ -1,4 +1,18 @@
 package org.emmfogo;
 
 public class Product {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void showProduct() {
+        System.out.printf("Product %s, price %d%n", this.name, this.price);
+    }
 }
